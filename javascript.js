6bcacabb1464a21bbf4d02769d0cf08412caa3de
cheyenneblue <32217561+cheyenneblue@users.noreply.github.com>
2018-02-14 17:37:21 +0300

    Add javascript.js
    Add drop-down menu and burger menu

---
+++ javascript.js
@@ -0,0 +1,21 @@
+document.querySelector("#nav-toggle")
+  .addEventListener("click", function () {
+    this.classList.toggle("active");
+  });
+
+
+$(function() {
+
+// Dropdown toggle //;
+
+  $('.dropdown-toggle').click(function(){
+    $(this).next('.dropdown').toggle();
+  });
+
+  $(document).click(function(e) {
+    var target = e.target;
+    if (!$(target).is('.dropdown-toggle') && !$(target).parents().is('.dropdown-toggle')) {
+      $('.dropdown').hide();
+    }
+  });
+});
